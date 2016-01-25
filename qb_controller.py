aec5f9a80b3259a2e410e24168ad8760c81dc908
hiankun <hiankun@gmail.com>
2016-01-25 15:38:21 +0800

    Add do_control() to take IR reading as the input to control the motor.\nThe controller is just a test one.

--- qb_controller.py
+++ qb_controller.py
@@ -58,15 +58,7 @@ def shutdown_motor():
     GPIO.cleanup()
     PWM.cleanup()
 
-
-if __name__ == '__main__':
-    import config
-
-    left_motor1, left_motor2, left_pwm = config.LEFT_MOTOR_PINS
-    right_motor1, right_motor2, right_pwm = config.RIGHT_MOTOR_PINS
-
-    setup_motor()
-
+def simple_motor_test():
     print '====== Testing Quick Bot ======='
     speed = 50 # if the speed is too low, the motor might be motionless
     print 'Run forward'
@@ -84,5 +76,37 @@ if __name__ == '__main__':
     print 'Stop'
     run_motor(0, 0)
     time.sleep(2)
+
+def get_ir_distance(ir_pins):
+    values = tuple(ADC.read_raw(pin) for pin in ir_pins)
+    # if the value is not available, assign -999
+    distances = map(lambda x: 2076.0/(x-11.0) if (x != 11.0) else -999., values)
+    #print '{:10.2f}{:10.2f}{:10.2f}{:10.2f}{:10.2f}'.format(*distances)
+    return distances
+
+def do_control(obstacles):
+    if (obstacles[1] < 5.0):
+        run_motor(50,-50)
+    elif (obstacles[3] < 5.0):
+        run_motor(-50,50)
+    else:
+        run_motor(50,50)
+
+if __name__ == '__main__':
+    import config
+
+    left_motor1, left_motor2, left_pwm = config.LEFT_MOTOR_PINS
+    right_motor1, right_motor2, right_pwm = config.RIGHT_MOTOR_PINS
+
+    setup_motor()
+
+    ADC.setup()
+    for _ in range(100):
+        distances = get_ir_distance(config.IR_PINS)
+        time.sleep(0.1)
+        do_control(distances)
+
+    run_motor(0,0)
+    time.sleep(1)
 
     shutdown_motor()
